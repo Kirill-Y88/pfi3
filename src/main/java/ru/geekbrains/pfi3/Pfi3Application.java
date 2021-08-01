46f01ce92318acbe195a6d3762e901db17a3c0a3
Kirill <benberov09@mail.ru>
2021-08-01 20:01:29 +0300

    Initial commit #1

---
+++ src/main/java/ru/geekbrains/pfi3/Pfi3Application.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.pfi3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pfi3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Pfi3Application.class, args);
+    }
+
+}
